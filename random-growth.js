b1671824a1252358e663d497dd022decebc16ce8
Brant Olsen <brantolsen@gmail.com>
2012-04-22 14:54:08 -0400

    Fix up javascript to work as a class.

--- random-growth.js
+++ random-growth.js
@@ -1,5 +1,35 @@
 
-function drawTrees(context, x, y, yStep, xStep, lifeCounter) {
+function RandomGrowth(context, settings)
+{
+  this.context = context;
+  this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+  var y = this.context.canvas.height;
+  var x = this.context.canvas.width / 2;
+  var yStep = Math.floor(y / 50);
+  if (yStep < 10)
+  {
+    yStep = 10;
+  }
+  var xStep = Math.floor(x / 50);
+  if (xStep < 10)
+  {
+    xStep = 10;
+  }
+  context.strokeStyle = "white";
+  console.log("x=" + x + ", y=" + y + ", ystep=" + yStep);
+  context.beginPath();
+  context.moveTo(x,y);
+  y = y - yStep * 10;
+  console.log("x=" + x + ", y=" + y + ", ystep=" + yStep);
+  context.lineTo(x,y);
+  context.stroke();
+
+  this.drawBranch(context, x, y, yStep, xStep, -1);
+  this.drawBranch(context, x, y, yStep, -xStep, -1);
+}
+
+RandomGrowth.prototype.drawBranch = function(context, x, y, yStep, xStep, lifeCounter) 
+{
   var percentFromSides = .05;
   if (lifeCounter == 0 ||
       y < context.canvas.height*percentFromSides ||
@@ -18,40 +48,18 @@ function drawTrees(context, x, y, yStep, xStep, lifeCounter) {
   context.lineTo(x,y);
   context.stroke();
 
-  drawTrees(context, x, y, yStep, xStep, lifeCounter);
+  this.drawBranch(context, x, y, yStep, xStep, lifeCounter);
   // This causes it to hang. Why?
   if (lifeCounter < 0)
   {
-    drawTrees(context, x, y, yStep, -xStep, 1);
-    drawTrees(context, x, y, yStep, xStep+xStep, 1);
+    this.drawBranch(context, x, y, yStep, -xStep, 1);
+    this.drawBranch(context, x, y, yStep, xStep+xStep, 1);
   }
   else
   {
     --lifeCounter;
-    drawTrees(context, x, y, yStep, -xStep, lifeCounter);
+    this.drawBranch(context, x, y, yStep, -xStep, lifeCounter);
   }
 }
 
-var y = canvas.height;
-var x = canvas.width / 2;
-var yStep = Math.floor(y / 50);
-if (yStep < 10)
-{
-  yStep = 10;
-}
-var xStep = Math.floor(x / 50);
-if (xStep < 10)
-{
-  xStep = 10;
-}
-context.strokeStyle = "white";
-console.log("x=" + x + ", y=" + y + ", ystep=" + yStep);
-context.beginPath();
-context.moveTo(x,y);
-y = y - yStep * 10;
-console.log("x=" + x + ", y=" + y + ", ystep=" + yStep);
-context.lineTo(x,y);
-context.stroke();
 
-drawTrees(context, x, y, yStep, xStep, -1);
-drawTrees(context, x, y, yStep, -xStep, -1);
